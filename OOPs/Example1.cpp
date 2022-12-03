bbad015ba30a893472aa7744663d061179ac2b2e
sagar <sagar-iitg>
2022-12-03 21:24:38 +0530

    Constructor

--- OOPs/Example1.cpp
+++ OOPs/Example1.cpp
@@ -3,15 +3,47 @@ using namespace std;
 
 class codehelp{
 
+
+
+        public:
+            int subs;
+            int views;
+
+        codehelp()
+        {
+            cout<<"I am in constructor"<<endl;
+
+
+        }
+
+
+        int getSubs()
+        {
+            return subs;
+
+        }
+
+        void setSubs(int value){
+            subs=value;
+        }
+
+        void print(){
+            cout<<subs<<endl;
+
+        }
 };
 
 int main()
 {
 
 
-    cout<<"hello";
-    std:cout<<sizeof(codehelp);
-
+    // cout<<"hello";
+    // std:cout<<sizeof(codehelp);
+    codehelp a;
+    codehelp *b=new codehelp();
     
+
+
+
 
 }
